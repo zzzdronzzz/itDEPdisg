4d7578b1b1e07347c6618874e42ee6e6578769a5
akatobi <sasacurakov@MacBook-Pro-Sasa.local>
2015-08-26 10:23:33 +0300

    update

--- selectRull.php
+++ selectRull.php
@@ -1,7 +1,58 @@
 <?php
+$groupSuUser = array(
+    '1' => 'sala',
+    '2' => 'otdelnov');
+$groupAxapta = array(
+    '1' => 'narkin',
+    '2' => 'klimov',
+    '3' => 'kruchinin',
+    '4' => 'kudryavceva'
+);
+$group1c = array(
+ '1' => "gagarin";
+);
+$groupFin = array(
+    '1' => 'krjachina'     
+);
+$groupSysAdmins = array(
+    '1' => 'appolonov',
+    '2' => 'girik',
+    '3' => 'grakov'
+);
+$fillip = array(
+    '1' =>'saarc'
+)
 
-
-
-
-
+    function RullGroup() {
+    foreach ($groupSuUser as $nickname) {
+        if ($_SESSION['user_id'] == $nickname) {
+            $_SESSION['$group'] = 'groupSuUser';
+        }
+    }
+    foreach($groupAxapta as $nickname) {
+       if ($_SESSION['user_id'] == $nickname) {
+            $_SESSION['$group'] = 'groupAxapta'; 
+    }
+    }
+    foreach($group1c as $nickname) {
+       if ($_SESSION['user_id'] == $nickname) {
+            $_SESSION['$group'] = 'group1c';
+    }
+    }
+    foreach($groupFin as $nickname) {
+       if ($_SESSION['user_id'] == $nickname) {
+            $_SESSION['$group'] = 'groupFin'; 
+    }
+    }
+    foreach($groupSysAdmins as $nickname) {
+       if ($_SESSION['user_id'] == $nickname) {
+            $_SESSION['$group'] = 'groupSysAdmins'; 
+    }
+    }
+    foreach($fillip as $nickname) {
+       if ($_SESSION['user_id'] == $nickname) {
+            $_SESSION['$group'] = 'fillip'; 
+    }
+}
+}
 ?>
